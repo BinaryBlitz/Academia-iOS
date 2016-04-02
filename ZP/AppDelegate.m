ca7702210f91ccda80bc5d43be82563cf958144a
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-04-02 15:45:53 +0300

    Set up Facebook SDK

--- ZP/AppDelegate.m
+++ ZP/AppDelegate.m
@@ -8,21 +8,19 @@
 
 #import "AppDelegate.h"
 
-#import <Crashlytics/Crashlytics.h>
-#import <Fabric/Fabric.h>
-//#import <DigitsKit/DigitsKit.h>
-#import "CocoaLumberjack.h"
+@import Fabric;
+@import Crashlytics;
+@import CocoaLumberjack;
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
 
 #import "ZPPUserManager.h"
-
-//#import <INTULocationManager/INTULocationManager.h>
 
 #define IS_OS_8_OR_LATER ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
 
 @import GoogleMaps;
 @import AFNetworking;
 
-NSString *const ZPPGMSApiKey = @"AIzaSyC97Zw13dQfFz3vI_k3fkSr7pYbyDqhohk";//AIzaSyC6__0DvrUgzEChAHV-_LRq6AwfDNOMJVM
+NSString *const ZPPGMSApiKey = @"AIzaSyC97Zw13dQfFz3vI_k3fkSr7pYbyDqhohk";
 
 @interface AppDelegate ()
 
@@ -32,7 +30,10 @@ NSString *const ZPPGMSApiKey = @"AIzaSyC97Zw13dQfFz3vI_k3fkSr7pYbyDqhohk";//AIza
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    [[FBSDKApplicationDelegate sharedInstance] application:application
+                             didFinishLaunchingWithOptions:launchOptions];
+    
     [DDLog addLogger:[DDASLLogger sharedInstance]];
     [DDLog addLogger:[DDTTYLogger sharedInstance]];
 
@@ -40,22 +41,6 @@ NSString *const ZPPGMSApiKey = @"AIzaSyC97Zw13dQfFz3vI_k3fkSr7pYbyDqhohk";//AIza
     [AFNetworkActivityIndicatorManager sharedManager].enabled = YES;
 
     [GMSServices provideAPIKey:ZPPGMSApiKey];
-
-    //    if ([launchOptions objectForKey:UIApplicationLaunchOptionsLocationKey]) {
-    //        INTULocationManager *locMgr = [INTULocationManager sharedInstance];
-    //        [locMgr
-    //            subscribeToSignificantLocationChangesWithBlock:^(CLLocation *currentLocation,
-    //                                                             INTULocationAccuracy
-    //                                                             achievedAccuracy,
-    //                                                             INTULocationStatus status){
-    //                // This block will be executed with the details of the significant location
-    //                change
-    //                // that triggered the background app launch,
-    //                // and will continue to execute for any future significant location change
-    //                events as
-    //                // well (unless canceled).
-    //            }];
-    //    }
 
     if (IS_OS_8_OR_LATER) {
         [application
@@ -93,15 +78,18 @@ NSString *const ZPPGMSApiKey = @"AIzaSyC97Zw13dQfFz3vI_k3fkSr7pYbyDqhohk";//AIza
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive.
-    // If the application was previously in the background, optionally refresh the user interface.
+    [FBSDKAppEvents activateApp];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also
-    // applicationDidEnterBackground:.
-    // Saves changes in the application's managed object context before the application terminates.
     [self saveContext];
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                          openURL:url
+                                                sourceApplication:sourceApplication
+                                                       annotation:annotation];
 }
 
 #pragma mark - Core Data stack
@@ -111,15 +99,11 @@ NSString *const ZPPGMSApiKey = @"AIzaSyC97Zw13dQfFz3vI_k3fkSr7pYbyDqhohk";//AIza
 @synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
 
 - (NSURL *)applicationDocumentsDirectory {
-    // The directory the application uses to store the Core Data store file. This code uses a
-    // directory named "ZPP.ZP" in the application's documents directory.
     return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory
                                                    inDomains:NSUserDomainMask] lastObject];
 }
 
 - (NSManagedObjectModel *)managedObjectModel {
-    // The managed object model for the application. It is a fatal error for the application not to
-    // be able to find and load its model.
     if (_managedObjectModel != nil) {
         return _managedObjectModel;
     }
@@ -129,8 +113,6 @@ NSString *const ZPPGMSApiKey = @"AIzaSyC97Zw13dQfFz3vI_k3fkSr7pYbyDqhohk";//AIza
 }
 
 - (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
-    // The persistent store coordinator for the application. This implementation creates and returns
-    // a coordinator, having added the store for the application to it.
     if (_persistentStoreCoordinator != nil) {
         return _persistentStoreCoordinator;
     }
@@ -155,9 +137,6 @@ NSString *const ZPPGMSApiKey = @"AIzaSyC97Zw13dQfFz3vI_k3fkSr7pYbyDqhohk";//AIza
         dict[NSLocalizedFailureReasonErrorKey] = failureReason;
         dict[NSUnderlyingErrorKey] = error;
         error = [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:9999 userInfo:dict];
-        // Replace this with code to handle the error appropriately.
-        // abort() causes the application to generate a crash log and terminate. You should not use
-        // this function in a shipping application, although it may be useful during development.
         NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
         abort();
     }
@@ -166,8 +145,6 @@ NSString *const ZPPGMSApiKey = @"AIzaSyC97Zw13dQfFz3vI_k3fkSr7pYbyDqhohk";//AIza
 }
 
 - (NSManagedObjectContext *)managedObjectContext {
-    // Returns the managed object context for the application (which is already bound to the
-    // persistent store coordinator for the application.)
     if (_managedObjectContext != nil) {
         return _managedObjectContext;
     }
@@ -189,17 +166,13 @@ NSString *const ZPPGMSApiKey = @"AIzaSyC97Zw13dQfFz3vI_k3fkSr7pYbyDqhohk";//AIza
     if (managedObjectContext != nil) {
         NSError *error = nil;
         if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
-            // Replace this implementation with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not
-            // use this function in a shipping application, although it may be useful during
-            // development.
             NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
             abort();
         }
     }
 }
 
-#pragma mark - push
+#pragma mark - Push notifications
 
 - (void)application:(UIApplication *)application
     didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
