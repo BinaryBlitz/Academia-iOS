c5e9b245ed2025a13a5c1bdee942f44665e19e9d
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-04-20 13:28:32 +0300

    Restore deleted cell
    Former-commit-id: ae4bc84e5c32107720ab461e516dd917fe11d2a0

---
+++ ZP/ZPPNoCreditCardCell.h
@@ -0,0 +1,14 @@
+//
+//  ZPPNoCreditCardCell.h
+//  ZP
+//
+//  Created by Andrey Mikhaylov on 02/11/15.
+//  Copyright © 2015 BinaryBlitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZPPNoCreditCardCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIButton *actionButton;
+
+@end
