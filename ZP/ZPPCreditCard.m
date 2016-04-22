d151046614aeb119cd2d6bbd9e69d6076d628f8e
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-04-22 13:46:13 +0300

    Add created date to card
    Former-commit-id: a89247fca6b2321226d49722620c777b621b063a

--- ZP/ZPPCreditCard.m
+++ ZP/ZPPCreditCard.m
@@ -7,12 +7,14 @@
 //
 
 #import "ZPPCreditCard.h"
+#import "NSDate+ZPPDateCategory.h"
 
 @interface ZPPCreditCard()
 
 @property (assign, nonatomic) NSInteger serverId;
 @property (strong, nonatomic) NSString *number;
 @property (strong, nonatomic) NSString *bindingId;
+@property (strong, nonatomic) NSDate *createdAt;
 
 @end
 
@@ -22,17 +24,19 @@
     NSNumber *serverId = dict[@"id"];
     NSString *cardNumber = dict[@"number"];
     NSString *bindingId = dict[@"binding_id"];
+    NSString *dateString = dict[@"created_at"];
     
-    ZPPCreditCard *card = [[ZPPCreditCard alloc] initWithCardNumber:cardNumber serverId:serverId.integerValue bindingId:bindingId];
+    ZPPCreditCard *card = [[ZPPCreditCard alloc] initWithCardNumber:cardNumber serverId:serverId.integerValue bindingId:bindingId createdAt:[NSDate customDateFromString:dateString]];
     return card;
 }
 
-- (instancetype)initWithCardNumber:(NSString *)cardNumber serverId:(NSInteger)serverId bindingId:(NSString *) bindingId {
+- (instancetype)initWithCardNumber:(NSString *)cardNumber serverId:(NSInteger)serverId bindingId:(NSString *) bindingId createdAt:(NSDate *)createdAt {
     self = [super init];
     if (self) {
         self.serverId = serverId;
         self.number = cardNumber;
         self.bindingId = bindingId;
+        self.createdAt = createdAt;
     }
     
     return self;
