a89247fca6b2321226d49722620c777b621b063a
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-04-22 13:46:13 +0300

    Add created date to card

--- ZP/ZPPCreditCard.h
+++ ZP/ZPPCreditCard.h
@@ -13,9 +13,10 @@
 @property (assign, nonatomic, readonly) NSInteger serverId;
 @property (strong, nonatomic, readonly) NSString *number;
 @property (strong, nonatomic, readonly) NSString *bindingId;
+@property (strong, nonatomic, readonly) NSDate *createdAt;
 
 + (ZPPCreditCard *)initWithDictionary: (NSDictionary *)dict;
 
-- (instancetype)initWithCardNumber:(NSString *)cardNumber serverId:(NSInteger)serverId bindingId:(NSString *) bindingId;
+- (instancetype)initWithCardNumber:(NSString *)cardNumber serverId:(NSInteger)serverId bindingId:(NSString *) bindingId createdAt:(NSDate *)createdAt;
 
 @end
