d8ffea8399282936533c37f5d81670278a481749
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-05-07 20:38:38 +0300

    Add total price detalisation
    Former-commit-id: 6c4795991aefcb1677113f7b66c23a856c6782f5

--- ZP/ZPPOrderTimeChooserVC.h
+++ ZP/ZPPOrderTimeChooserVC.h
@@ -13,10 +13,12 @@
 @interface ZPPOrderTimeChooserVC : UIViewController
 @property (weak, nonatomic) IBOutlet UIButton *nowButton;
 @property (weak, nonatomic) IBOutlet UIButton *atTimeButton;
-@property (weak, nonatomic) IBOutlet UILabel *totalPriceLabel;
 @property (weak, nonatomic) IBOutlet UIButton *makeOrderButton;
-@property (weak, nonatomic) IBOutlet UILabel *deliveryLabel;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *nowButtonHeight;
+@property (weak, nonatomic) IBOutlet UILabel *totalPriceWithDelivery;
+@property (weak, nonatomic) IBOutlet UILabel *balanceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *discountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalPriceLabel;
 
 - (void)configureWithOrder:(ZPPOrder *)order;
 
