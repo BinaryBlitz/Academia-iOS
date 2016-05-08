9a416406c6827a2ac742f7f643899318605cbdd9
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-05-08 17:02:04 +0300

    Fix total prica calculation and edit price details labels
    Former-commit-id: 72678ee4c30dd40130bd5d389470c420ea91bbe3

--- ZP/ZPPOrderTimeChooserVC.h
+++ ZP/ZPPOrderTimeChooserVC.h
@@ -9,16 +9,15 @@
 @import UIKit;
 
 @class ZPPOrder;
+@class OAStackView;
 
 @interface ZPPOrderTimeChooserVC : UIViewController
 @property (weak, nonatomic) IBOutlet UIButton *nowButton;
 @property (weak, nonatomic) IBOutlet UIButton *atTimeButton;
 @property (weak, nonatomic) IBOutlet UIButton *makeOrderButton;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *nowButtonHeight;
-@property (weak, nonatomic) IBOutlet UILabel *totalPriceWithDelivery;
-@property (weak, nonatomic) IBOutlet UILabel *balanceLabel;
-@property (weak, nonatomic) IBOutlet UILabel *discountLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalPriceLabel;
+@property (weak, nonatomic) IBOutlet OAStackView *totalPriceDetailsStackView;
 
 - (void)configureWithOrder:(ZPPOrder *)order;
 
