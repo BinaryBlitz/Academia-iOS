0eb3087800e73c531ac1fd91b14ffbb656de1b25
Dan Kim <itsdanya@gmail.com>
2017-02-24 01:26:00 +0300

    Update welcome screen provider

--- ZP/WelcomeScreenProvider.swift
+++ ZP/WelcomeScreenProvider.swift
@@ -10,13 +10,13 @@ import Foundation
 
 @objc class WelcomeScreenProvider: NSObject {
 
-    static var sharedProvider = WelcomeScreenProvider()
+  static var sharedProvider = WelcomeScreenProvider()
 
-    var imageURLString: String?
-    var imageURL: URL? {
-        return URL(string: (imageURLString ?? ""))
-    }
-    var hasAvailableScreen: Bool {
-        return imageURLString != nil
-    }
+  var imageURLString: String?
+  var imageURL: URL? {
+    return URL(string: (imageURLString ?? ""))
+  }
+  var hasAvailableScreen: Bool {
+    return imageURLString != nil
+  }
 }
